ba2b124e9cf54eadee32285bf0503bf088fb1ec3
Guilhem Saurel <guilhem.saurel@laas.fr>
2024-06-28 14:15:20 +0200

    nix: package

--- flake.nix
+++ flake.nix
@@ -1,21 +1,23 @@
 {
-  description = "Description for the project";
+  description = "Utility classes to check the (robust) equilibrium of a system in contact with the environment.";
+
+  nixConfig = {
+    extra-substituters = [ "https://gepetto.cachix.org" ];
+    extra-trusted-public-keys = [ "gepetto.cachix.org-1:toswMl31VewC0jGkN6+gOelO2Yom0SOHzPwJMY2XiDY=" ];
+  };
 
   inputs = {
-    flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:nim65s/nixpkgs/gepetto";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
   };
 
   outputs =
     inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
-      imports = [
-        # To import a flake module
-        # 1. Add foo to inputs
-        # 2. Add foo as a parameter to the outputs function
-        # 3. Add here: foo.flakeModule
-
-      ];
+      imports = [ ];
       systems = [
         "x86_64-linux"
         "aarch64-linux"
@@ -23,27 +25,10 @@
         "x86_64-darwin"
       ];
       perSystem =
+        { self', pkgs, ... }:
         {
-          config,
-          self',
-          inputs',
-          pkgs,
-          system,
-          ...
-        }:
-        {
-          # Per-system attributes can be defined here. The self' and inputs'
-          # module parameters provide easy access to attributes of the same
-          # system.
-
-          # Equivalent to  inputs'.nixpkgs.legacyPackages.hello;
-          packages.default = pkgs.hello;
+          packages.default = pkgs.callPackage ./. { };
+          devShells.default = pkgs.mkShell { inputsFrom = [ self'.packages.default ]; };
         };
-      flake = {
-        # The usual flake attributes can be defined here, including system-
-        # agnostic ones like nixosModule and system-enumerating ones, although
-        # those are more easily expressed in perSystem.
-
-      };
     };
 }
