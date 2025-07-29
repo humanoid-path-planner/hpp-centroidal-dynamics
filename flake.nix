97a8a4337cbbde708a1adff1d5815e02b217f5db
Guilhem Saurel <guilhem.saurel@laas.fr>
2025-07-29 15:50:38 +0200

    Nix: update gepetto/nix use

--- flake.nix
+++ flake.nix
@@ -2,36 +2,33 @@
   description = "Utility classes to check the (robust) equilibrium of a system in contact with the environment.";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/refs/pull/362956/head";
-    flake-parts = {
-      url = "github:hercules-ci/flake-parts";
-      inputs.nixpkgs-lib.follows = "nixpkgs";
-    };
+    gepetto.url = "github:gepetto/nix";
+    flake-parts.follows = "gepetto/flake-parts";
+    nixpkgs.follows = "gepetto/nixpkgs";
+    nix-ros-overlay.follows = "gepetto/nix-ros-overlay";
+    systems.follows = "gepetto/systems";
+    treefmt-nix.follows = "gepetto/treefmt-nix";
   };
 
   outputs =
     inputs:
     inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [
-        "x86_64-linux"
-        "aarch64-linux"
-        "aarch64-darwin"
-        "x86_64-darwin"
-      ];
+      systems = import inputs.systems;
+      imports = [ inputs.gepetto.flakeModule ];
       perSystem =
-        { pkgs, self', ... }:
         {
-          apps.default = {
-            type = "app";
-            program = pkgs.python3.withPackages (_: [ self'.packages.default ]);
-          };
-          devShells.default = pkgs.mkShell { inputsFrom = [ self'.packages.default ]; };
+          lib,
+          pkgs,
+          self',
+          ...
+        }:
+        {
           packages = {
             default = self'.packages.hpp-centroidal-dynamics;
-            hpp-centroidal-dynamics = pkgs.python3Packages.hpp-centroidal-dynamics.overrideAttrs (_: {
-              src = pkgs.lib.fileset.toSource {
+            hpp-centroidal-dynamics = pkgs.python3Packages.hpp-centroidal-dynamics.overrideAttrs {
+              src = lib.fileset.toSource {
                 root = ./.;
-                fileset = pkgs.lib.fileset.unions [
+                fileset = lib.fileset.unions [
                   ./CMakeLists.txt
                   ./include
                   ./package.xml
@@ -41,7 +38,7 @@
                   ./test_data
                 ];
               };
-            });
+            };
           };
         };
     };
